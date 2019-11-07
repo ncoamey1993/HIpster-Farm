cb527a785d7df89a5eacf87af3bae01058fa7518
ncoamey1993 <51166116+ncoamey1993@users.noreply.github.com>
2019-11-07 11:32:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("side-bar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("side-bar").style.width = "0";
+}
+
+function changeColor() {
+    document.style.background = "#947e70";
+}
